5b66ba0476ed31ba6f2c111f5fbbe95383afee6d
chomeed <cmeedeum@gmail.com>
2024-01-04 03:58:34 +0900

    second commit

--- prepare_dataset.sh
+++ prepare_dataset.sh
@@ -1,13 +1,16 @@
+# bash jejuSeg/prepare_dataset
+
 # unzip dataset zip files 
-unzip aihub_jeju/train/labels/FGT_1024.zip -d /path/to/mmsegmentation/data/train/labels
-unzip aihub_jeju/val/labels/FGT_1024.zip -d /path/to/mmsegmentation/data/val/labels
-unzip aihub_jeju/train/images/FGT_1024.zip -d /path/to/mmsegmentation/data/train/images
-unzip aihub_jeju/val/images/FGT_1024.zip -d /path/to/mmsegmentation/data/val/images
+cd jejuSeg
+unzip /dataset/jeju/train/labels/FGT_1024.zip -d mmsegmentation/data/train/labels
+unzip /dataset/jeju/val/labels/FGT_1024.zip -d mmsegmentation/data/val/labels
+unzip /dataset/jeju/train/images/FGT_1024.zip -d mmsegmentation/data/train/images
+unzip /dataset/jeju/val/images/FGT_1024.zip -d mmsegmentation/data/val/images
 
 # geo2coco 
-python geo2coco.py /path/to/mmsegmentation/data/train/labels/FGT_1024/json /path/to/mmsegmentation/data/train/train.json
+python geo2coco.py mmsegmentation/data/train/labels/FGT_1024/json mmsegmentation/data/train/train.json
 # python geo2coco.py mmsegmentation/data/train/labels/FGT_1024/json mmsegmentation/data/train/train.json
-python geo2coco.py /path/to/mmsegmentation/data/val/labels/FGT_1024/json /path/to/mmsegmentation/data/val/val.json
+python geo2coco.py mmsegmentation/data/val/labels/FGT_1024/json mmsegmentation/data/val/val.json
 # python geo2coco.py mmsegmentation/data/val/labels/FGT_1024/json mmsegmentation/data/val/val.json
 
 # create mask files 
@@ -18,4 +21,4 @@ python maskcoco.py mmsegmentation/data/val/val.json mmsegmentation/data/val/mask
 # train 
 cd mmsegmentation 
 # python tools/train.py configs/_ours_/unet-s5-d16_deeplabv3_40k_jeju.py --work-dir experiments
-python tools/train.py configs/_ours_/segformer-b0_160k_jeju.py --work-dir experiments
+python tools/train.py configs/_ours_/segformer-b0_160k_jeju.py --work-dir /output
